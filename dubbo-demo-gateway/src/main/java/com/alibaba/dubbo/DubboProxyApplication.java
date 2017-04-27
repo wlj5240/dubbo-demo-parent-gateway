081d533672ac143801b553c657836b4d8b7bf3cf
wuyu <191287278@qq.com>
2017-04-27 11:53:21 +0800

    init

--- dubbo-demo-gateway/src/main/java/com/alibaba/dubbo/DubboProxyApplication.java
+++ dubbo-demo-gateway/src/main/java/com/alibaba/dubbo/DubboProxyApplication.java
@@ -9,9 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @EnableDubboProxy
-public class DubboApplication {
+public class DubboProxyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboApplication.class, args);
+        SpringApplication.run(DubboProxyApplication.class, args);
     }
 }
