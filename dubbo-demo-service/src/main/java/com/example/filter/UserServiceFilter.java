2e93410cb84a6b90acbb515a8b4216439d3addaf
wuyu <191287278@qq.com>
2017-04-27 14:26:56 +0800

    init

--- dubbo-demo-service/src/main/java/com/example/filter/UserServiceFilter.java
+++ dubbo-demo-service/src/main/java/com/example/filter/UserServiceFilter.java
@@ -3,10 +3,15 @@ package com.example.filter;
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.rpc.*;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
-@Activate(group = {Constants.PROVIDER, Constants.CONSUMER})
+@Activate(group = Constants.PROVIDER)
 public class UserServiceFilter implements Filter {
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
